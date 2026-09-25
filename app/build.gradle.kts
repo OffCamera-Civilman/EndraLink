53d44c456625bb1faa2a88549d1e3291134dcdd6
OffCamera-Civilman <dextersgray9@gmail.com>
2026-09-25 01:22:26 -0400

    Bump EndraLink to 0.1.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.endralink.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.1.11"
+        versionCode = 13
+        versionName = "0.1.12"
     }
 }
 dependencies {
